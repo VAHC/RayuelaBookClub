0a310d0244e4e1d809fc0350e8282df73ecbc3c7
Noelia <noelia.martino@live.com.ar>
2023-05-18 19:52:46 -0300

    estructura y mas componentes basicos

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,20 +1,26 @@
-import './App.css'
+//import './App.css'
 
 //Librerias
 import {Route, Routes} from "react-router-dom";
 
 //Componentes
-import {Home} from "./components/home/Home"
+import { Home } from "./components/home/Home";
+import { Nav } from "./components/nav/Nav";
 import { Catalogo } from './components/catalogo/catalogo';
+import { Suscripcion } from './components/suscripcion/Suscripcion';
+import { Footer } from "./components/footer/Footer";
 
 
 function App() {
   return (
     <>
+      <Nav/>
       <Routes>
-        <Route exact path="/home" element={<Home/>}/>
+        <Route exact path="/" element={<Home/>}/>
         <Route exact path="/catalogo" element={<Catalogo/>}/>
+        <Route exact path="/suscripcion" element={<Suscripcion/>}/>
       </Routes>
+      <Footer/>
     </>
   )
 }
