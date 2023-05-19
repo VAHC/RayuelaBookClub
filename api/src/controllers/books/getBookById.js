06120f65011af5d63e6a388ea20cff8a685ba8c7
VAHC <victor10alha@gmail.com>
2023-05-19 20:55:23 -0300

    creación de getById del Detail

--- api/src/controllers/books/getBookById.js
+++ api/src/controllers/books/getBookById.js
@@ -1,5 +1,32 @@
-const getBookById = (id) => {
-  return 'funciono'
-}
+const {Book} = require('../../db')
 
-module.exports = { getBookById }
+const getBookById = async (id) => {
+    const foundBook = await Book.findByPk(id, {
+      
+    });
+
+    if (!foundBook) throw new Error(`Book not found with ID: ${id}`);
+    return foundBook;
+  };
+
+  module.exports = {getBookById};
+
+  // const getBookById = async (id) => {
+  //   const foundBook = await Book.findByPk(id, {
+  //     include: {
+  //       model: Gender,
+  //       through: {
+  //         attributes: [],
+  //       },
+  //     },
+  //     {
+  //       model: Author,
+  //       through: {
+  //         attributes: [],
+  //       },
+  //     },
+  //   });
+
+  //   if (!foundBook) throw new Error(`Book not found with ID: ${id}`);
+  //   return foundBook;
+  // };
