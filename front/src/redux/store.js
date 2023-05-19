2b9778e042b9a235d1ce82eb37083e61e8809313
Noelia <noelia.martino@live.com.ar>
2023-05-19 19:47:34 -0300

    home base

--- front/src/redux/store.js
+++ front/src/redux/store.js
@@ -1,7 +1,7 @@
-import {applyMiddleware, createStore, compose} from 'redux'
+import {applyMiddleware, createStore, compose} from 'redux';
 
-import reducer from './reducer'
-import thunk from 'redux-thunk'
+import reducer from './reducer';
+import thunk from 'redux-thunk';
 //import { composeWithDevTools } from 'redux-devtools-extension' 
 //(NPM I 'redux-devtools-extension') IN ORDER TO USE THIS METHOD
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
