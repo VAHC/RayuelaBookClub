27a31c801a71a3198735710a414edf1a369d8e07
Noelia <noelia.martino@live.com.ar>
2023-05-20 16:32:37 -0300

    componente about

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,18 +1,17 @@
 //Librerias
 import {Route, Routes} from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //Componentes
 import { Home } from "./components/home/Home";
 import { Nav } from "./components/nav/Nav";
 import { Catalogo } from './components/catalogo/catalogo';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 import BookList from './components/home/BookList'
 
 import { Suscripcion } from './components/suscripcion/Suscripcion';
 import { Footer } from "./components/footer/Footer";
-
-
+import { AboutUs } from "./components/aboutUs/AboutUs";
 
 function App() {
   return (
@@ -20,12 +19,11 @@ function App() {
       <Nav/>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/nosotros" element={<AboutUs/>}/>
+        <Route exact path="/suscripcion" element={<Suscripcion/>}/>
         <Route exact path="/catalogo" element={<Catalogo/>}/>
 
         <Route exact path="/books" element={<BookList />}/>
-
-        <Route exact path="/suscripcion" element={<Suscripcion/>}/>
-
       </Routes>
       <Footer/>
     </>
