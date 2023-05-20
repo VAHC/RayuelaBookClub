8d1c853d98333792b81a04be629dd9e0704fc385
Noelia <noelia.martino@live.com.ar>
2023-05-20 01:40:24 -0300

    footer basico y suscripcion base

--- front/src/components/nav/Nav.jsx
+++ front/src/components/nav/Nav.jsx
@@ -13,24 +13,12 @@ export const Nav = () => {
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
           </button>
-          <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
-              <li className="nav-item">
+          <div className="collapse navbar-collapse nav-fill" id="navbarNav">
                 <Link to="/" className="nav-link fs-4 nav-link active" aria-current="page">Inicio</Link>
-              </li>
-              <li className="nav-item">
                 <Link to="/" className="nav-link fs-4">Nosotros</Link>
-              </li>
-              <li className="nav-item">
                 <Link to="/suscripcion" className="nav-link fs-4">Suscribirme</Link>
-              </li>
-              <li className="nav-item">
                 <Link to="/catalogo" className="nav-link fs-4">Tienda</Link>
-              </li>
-              <li className="nav-item">
                 <Link to="/" className="nav-link fs-4">Ingresar</Link>
-              </li>
-            </ul>
           </div>
         </div>
       </nav>
