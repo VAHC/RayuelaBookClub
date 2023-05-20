6400c081fa255a077695386eb6676af5a6e8cd4b
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-05-20 19:58:47 -0300

    estilos orden

--- front/src/components/catalogo/Orders.jsx
+++ front/src/components/catalogo/Orders.jsx
@@ -21,23 +21,25 @@ export const Orders = () => {
   return (
     <div>
       <h6>Ordena por</h6>
-      <div>
-        {/* <label htmlFor='priceOrder'>Precio:</label> */}
-        <select className='form-select' name='priceOrder' value={priceValue} onChange={handlerSortByPrice}>
-          {/* <option value="" readOnly hidden>Elije una opcion...</option> */}
-          <option value="" readOnly hidden>Precio...</option>
-          <option value="Asc">Menor precio</option>
-          <option value="Desc">Mayor precio</option>
-        </select>
-      </div>
-      <div>
-        {/* <label htmlFor='ratingOrder'>Valoracion:</label> */}
-        <select className='form-select' name='ratingOrder' value={ratingValue} onChange={handlerSortByRating}>
-          {/* <option value="" readOnly hidden>Elije una opcion...</option> */}
-          <option value="" readOnly hidden>Valoracion...</option>
-          <option value="Asc">Peor Valorado</option>
-          <option value="Desc">Mejor Valorado</option>
-        </select>
+      <div className='input-group mb-3'>
+        <div>
+          {/* <label htmlFor='priceOrder'>Precio:</label> */}
+          <select className='form-select' name='priceOrder' value={priceValue} onChange={handlerSortByPrice}>
+            {/* <option value="" readOnly hidden>Elije una opcion...</option> */}
+            <option value="" readOnly hidden>Precio...</option>
+            <option value="Asc">Menor precio</option>
+            <option value="Desc">Mayor precio</option>
+          </select>
+        </div>
+        <div>
+          {/* <label htmlFor='ratingOrder'>Valoracion:</label> */}
+          <select className='form-select' name='ratingOrder' value={ratingValue} onChange={handlerSortByRating}>
+            {/* <option value="" readOnly hidden>Elije una opcion...</option> */}
+            <option value="" readOnly hidden>Valoracion...</option>
+            <option value="Asc">Peor Valorado</option>
+            <option value="Desc">Mejor Valorado</option>
+          </select>
+        </div>
       </div>
     </div>
   )
