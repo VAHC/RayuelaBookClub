1888a4d989d39c7547f0a6f4adb435053e71424c
LucasFerraro <ferrarolucasmartin@gmail.com>
2023-05-21 07:59:09 -0300

    Refactor(components/Card & Catalogo & Detail & Posters) El catalogo esta linkeado con el detail y el reducer. Los ordenes se trasladaron al sector de filtros.

--- front/src/components/catalogo/Detail.jsx
+++ front/src/components/catalogo/Detail.jsx
@@ -1,75 +1,80 @@
 import React from "react";
 import { Card, Button, Row, Col } from "react-bootstrap";
-import {useSelector} from "react-redux"
+import { useSelector } from "react-redux";
 
 export const Detail = () => {
-const detailData = useSelector((state) => state.detail_data)
+    const detailData = useSelector((state) => state.detail_data);
 
-    return (
+    if (!detailData){return(
         <Card style={{ width: "90%", height: "100%" }}>
-            <Card.Body className="d-flex flex-column justify-content-evenly">
-                <div
-                    style={{
-                        display: "flex",
-                        alignItems: "center"
-                    }}
-                >
-                    
-                    <h4 style={{ marginRight: "10px", maxWidth: "70%" }}> 
-                    {/* {detailData.title} */}
-                        Este es el Título del libro, puede ser Largo{" "}
-                    </h4>
-                    <Button
-                        variant="secondary"
+        <Card.Body className="d-flex flex-column justify-content-evenly">
+     <h1>Selecciona un libro para ver mas detalles.</h1>
+        </Card.Body>
+    </Card>
+    )}
+    else{
+        return (
+            <Card style={{ width: "90%", height: "100%" }}>
+                <Card.Body className="d-flex flex-column justify-content-evenly">
+                    <div
                         style={{
-                            borderRadius: "50%",
-                            width: "70px",
-                            height: "70px",
+                            display: "flex",
+                            alignItems: "center",
                         }}
                     >
-                        5/5 ★
-                    </Button>
-                </div>
-                <Row>
-                    <Col>
-                        <Card.Subtitle className="mb-2 text-muted">
-                            Autor
-                        </Card.Subtitle>
-                        {/* {detailData.author.map((author)=>{return <Card.Text>author</Card.Text>})} */}
-                        <Card.Text>
-                            Autor del Libro</Card.Text>
-                    </Col>
-                    <Col>
-                        <Card.Subtitle className="mb-2 text-muted">
-                            Género
-                        </Card.Subtitle>
-                        {/* {detailData.gender.map((gender)=>{return <Card.Text>gender</Card.Text>})} */}
-                        <Card.Text>
-                            Genero del Libro</Card.Text>
-                    </Col>
-                </Row>
-
-                <Card.Text>
-                    {/* {detailData.description} */}
-                    Descripción del libro que puede tener varias líneas para
-                    mostrar más información sobre el contenido.
-                </Card.Text>
-
-                <div>
+                        <h4 style={{ marginRight: "10px", maxWidth: "70%" }}>
+                            {detailData.title}
+                        </h4>
+                        <Button
+                            variant="secondary"
+                            style={{
+                                borderRadius: "50%",
+                                width: "70px",
+                                height: "70px",
+                            }}
+                        >
+                            5/5 ★
+                        </Button>
+                    </div>
                     <Row>
                         <Col>
-                            <Button variant="primary">
-                                Agregar al carrito
-                            </Button>
+                            <Card.Subtitle className="text-muted">
+                                Autor
+                            </Card.Subtitle>
+                            {detailData.authors.map((author) => {
+                                return <Card.Text>{author}</Card.Text>;
+                            })}
                         </Col>
                         <Col>
-                            <Button variant="outline-secondary">
-                                Agregar a la Wishlist
-                            </Button>
+                            <Card.Subtitle className="text-muted">
+                                Género
+                            </Card.Subtitle>
+                            {detailData.gender.map((gender) => {
+                                return <Card.Text>{gender}</Card.Text>;
+                            })}
                         </Col>
                     </Row>
-                </div>
-            </Card.Body>
-        </Card>
-    );
+
+                    <Card.Text style={{ overflow: "auto", height: "200px" }}>
+                        {detailData.description}
+                    </Card.Text>
+
+                    <div>
+                        <Row>
+                            <Col>
+                                <Button variant="primary">
+                                    Agregar al carrito
+                                </Button>
+                            </Col>
+                            <Col>
+                                <Button variant="outline-secondary">
+                                    Agregar a la Wishlist
+                                </Button>
+                            </Col>
+                        </Row>
+                    </div>
+                </Card.Body>
+            </Card>
+        );
+    }
 };
