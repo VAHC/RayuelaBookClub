d4f4d101149acfbaacd0d6e11692dc8a9512daaa
VAHC <victor10alha@gmail.com>
2023-05-20 22:56:23 -0300

    modifico getBookById para autor y género

--- api/src/controllers/books/getBookById.js
+++ api/src/controllers/books/getBookById.js
@@ -2,31 +2,26 @@ const {Book} = require('../../db')
 
 const getBookById = async (id) => {
     const foundBook = await Book.findByPk(id, {
-      
+      include: [{
+        model: Gender,
+        through: {
+          attributes: [],
+        }
+      },
+      {
+        model: Author,
+        through: {
+          attributes: [],
+        },
+      }
+    ]
     });
 
     if (!foundBook) throw new Error(`Book not found with ID: ${id}`);
     return foundBook;
   };
 
+
   module.exports = {getBookById};
 
-  // const getBookById = async (id) => {
-  //   const foundBook = await Book.findByPk(id, {
-  //     include: {
-  //       model: Gender,
-  //       through: {
-  //         attributes: [],
-  //       },
-  //     },
-  //     {
-  //       model: Author,
-  //       through: {
-  //         attributes: [],
-  //       },
-  //     },
-  //   });
-
-  //   if (!foundBook) throw new Error(`Book not found with ID: ${id}`);
-  //   return foundBook;
-  // };
+  
