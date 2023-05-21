01aa1ba233d94b4c120b47110c58fc13a0cbe0ad
Andrés Carola <100820494+andrescarola@users.noreply.github.com>
2023-05-21 12:54:55 -0300

    se modifica alternativa de search bar

--- api/src/controllers/books/searchBooks.js
+++ api/src/controllers/books/searchBooks.js
@@ -2,50 +2,21 @@ const getAllBooks = require('./getAllBooks');
 
 // Esta función NO distingue si lo que se busca es un title o un authors
 
-// const searchBooks = async (search) => {
-//     const allBooks = await getAllBooks();
-
-//     if (search){
-//         console.log(search)
-//         const booksFiltered = allBooks.filter((book) => {
-
-//             const lowerCaseSearch = search.toLowerCase();
-           
-//             return (
-//                 book.title.toLowerCase().includes(lowerCaseSearch) ||
-//                 book.authors.some((author) => author.toLowerCase().includes(lowerCaseSearch))
-//             )
-//         })
-    
-//         if (booksFiltered.length === 0) {
-//             throw Error(`No books have been found matching your search:'${search}'`)
-//         } else {
-//         return booksFiltered;
-//     }
-// }
-// };
-
-
-// Esta función SÍ distingue si lo que se busca es un title o un authors.
-
-const searchBooks = async (search, searchField) => {
+const searchBooks = async (search) => {
     const allBooks = await getAllBooks();
 
-    console.log(search)
-    console.log(searchField)
-
     if (search){
-        const lowerCaseSearch = search.toLowerCase();
-
+        console.log(search)
         const booksFiltered = allBooks.filter((book) => {
 
-            if (searchField === 'title') {
-                return book.title.toLowerCase().includes(lowerCaseSearch)
-            } if (searchField === 'authors') {
-                return book.authors.some((author) => author.toLowerCase().includes(lowerCaseSearch))
-            }
+            const lowerCaseSearch = search.toLowerCase();
+           
+            return (
+                book.title.toLowerCase().includes(lowerCaseSearch) ||
+                book.authors.some((author) => author.toLowerCase().includes(lowerCaseSearch))
+            )
         })
-
+    
         if (booksFiltered.length === 0) {
             throw Error(`No books have been found matching your search:'${search}'`)
         } else {
@@ -53,5 +24,34 @@ const searchBooks = async (search, searchField) => {
     }
 }
 };
+
+
+// Esta función SÍ distingue si lo que se busca es un title o un authors.
+
+// const searchBooks = async (search, searchField) => {
+//     const allBooks = await getAllBooks();
+
+//     console.log(search)
+//     console.log(searchField)
+
+//     if (search){
+//         const lowerCaseSearch = search.toLowerCase();
+
+//         const booksFiltered = allBooks.filter((book) => {
+
+//             if (searchField === 'title') {
+//                 return book.title.toLowerCase().includes(lowerCaseSearch)
+//             } if (searchField === 'authors') {
+//                 return book.authors.some((author) => author.toLowerCase().includes(lowerCaseSearch))
+//             }
+//         })
+
+//         if (booksFiltered.length === 0) {
+//             throw Error(`No books have been found matching your search:'${search}'`)
+//         } else {
+//         return booksFiltered;
+//     }
+// }
+// };
 
 module.exports = searchBooks;
