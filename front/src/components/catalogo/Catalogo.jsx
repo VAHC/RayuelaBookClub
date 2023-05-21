d6edd1e576c2f95e80cbe3d65a368ac5132ce7e3
LucasFerraro <ferrarolucasmartin@gmail.com>
2023-05-21 08:37:42 -0300

    newCode(components)creacion del paginado, en funcionamiento

--- front/src/components/catalogo/Catalogo.jsx
+++ front/src/components/catalogo/Catalogo.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect }  from "react";
+import React, { useEffect } from "react";
 
 //Componentes
 import { Detail } from "./Detail";
@@ -6,6 +6,7 @@ import { SearchBar } from "./SearchBar";
 import { Orders } from "./Orders";
 import { Filters } from "./Filters";
 import { Posters } from "./Posters";
+import { Paginado } from "./Paginado";
 
 //Estilos
 import {
@@ -15,49 +16,45 @@ import {
     SearchBarDiv,
     DetailSection,
     PosterSection,
-    Wrap
+    Wrap,
 } from "./Styles/catalogo";
 
-import {getAllBooks } from "../../redux/action";
-import {useDispatch, useSelector } from "react-redux";
+import { getAllBooks } from "../../redux/action";
+import { useDispatch, useSelector } from "react-redux";
 
 // En este componente se renderizan todos los demas.
 export const Catalogo = () => {
-
     const dispatch = useDispatch();
-    
+
     useEffect(() => {
         dispatch(getAllBooks());
         console.log("se hace el dispatch para buscar allBooks");
-    }, [])
+    }, []);
 
-    const detailData= useSelector(state=>state.detailData)
+    const detailData = useSelector((state) => state.detailData);
 
     return (
         <Wrap>
             {/* ¿NavBar? */}
-
-            <SearchBarDiv>
-                <SearchBar />
-            </SearchBarDiv>
-
             <Container>
                 <Sidebar>
-                <Orders />
+                    <Orders />
                     <Filters />
                 </Sidebar>
 
                 <CatalogoSection>
-
+                    <SearchBar />
+                    <Paginado/>
                     <PosterSection>
                         <Posters />
                     </PosterSection>
-
                 </CatalogoSection>
 
-                {detailData === undefined  && <DetailSection>
-                    <Detail />
-                </DetailSection>}
+                {detailData === undefined && (
+                    <DetailSection>
+                        <Detail />
+                    </DetailSection>
+                )}
             </Container>
         </Wrap>
     );
