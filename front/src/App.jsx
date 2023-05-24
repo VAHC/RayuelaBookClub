e31dd2dd0b3a74d3a005d2608152f6a6374b3f81
LucasFerraro <ferrarolucasmartin@gmail.com>
2023-05-24 09:03:53 -0300

    Refactor(app//components//redux) Elimine el codigo de Kelvin

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -7,8 +7,6 @@ import { Home } from "./components/home/Home";
 import { Nav } from "./components/nav/Nav";
 import { Catalogo } from './components/catalogo/catalogo';
 
-import BookList from './components/home/BookList'
-
 import { Suscripcion } from './components/suscripcion/Suscripcion';
 import { Footer } from "./components/footer/Footer";
 import { AboutUs } from "./components/aboutUs/AboutUs";
@@ -24,7 +22,6 @@ function App() {
         <Route exact path="/suscripcion" element={<Suscripcion/>}/>
         <Route exact path="/catalogo" element={<Catalogo/>}/>
         <Route exact path="/formulario" element={<FormCreateBook/>}/>
-        <Route exact path="/books" element={<BookList />}/>
       </Routes>
       <Footer/>
     </>
