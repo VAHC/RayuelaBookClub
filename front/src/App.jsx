fdc9c19b16781f94425789d46cad67bfede84d7c
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-05-25 02:14:43 -0300

    rutas reviews e import app

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -6,12 +6,15 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { Home } from "./components/home/Home";
 import { Nav } from "./components/nav/Nav";
 import { Catalogo } from './components/catalogo/catalogo';
-
 import { Suscripcion } from './components/suscripcion/Suscripcion';
 import { Footer } from "./components/footer/Footer";
 import { AboutUs } from "./components/aboutUs/AboutUs";
 import { FormCreateBook } from "./components/formCreateBook/formCreateBook";
-import Dashboard from "./components/adminDashboard/Dashboard"
+import Dashboard from "./components/adminDashboard/Dashboard";
+import ContainerReviews from './components/reviews/ContainerReviews';
+import FormCreateReview from './components/reviews/FormCreateReview';
+import MyReviews from './components/reviews/myReviews/MyReviews';
+import FormEditReviews from './components/reviews/myReviews/FormEditReviews'
 
 function App() {
   return (
@@ -24,10 +27,10 @@ function App() {
         <Route exact path="/catalogo" element={<Catalogo/>}/>
         <Route exact path="/formulario" element={<FormCreateBook/>}/>
         <Route exact path="/dashboard" element={<Dashboard/>}/>
-        {/* <Route exact path="/reviews" element={<ContainerReviews/>}/>
+        <Route exact path="/reviews" element={<ContainerReviews/>}/>
         <Route exact path="/createReviews" element={<FormCreateReview/>}/>
         <Route exact path="/myReviews" element={<MyReviews/>}/>
-        <Route exact path="/editReviews" element={<FormEditReviews/>}/> */}
+        <Route exact path="/editReviews" element={<FormEditReviews/>}/>
       </Routes>
       <Footer/>
     </>
