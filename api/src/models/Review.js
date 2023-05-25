0225860b19ed47e863a002715d3b33d3e3750334
VAHC <victor10alha@gmail.com>
2023-05-25 15:40:07 -0300

    create models user, review + relaciones

---
+++ api/src/models/Review.js
@@ -0,0 +1,34 @@
+const { DataTypes } = require('sequelize')
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('review', {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
+    },
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    qualification: {
+      type: DataTypes.INTEGER,
+      allowNull: false
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    createdDb: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+      defaultValue: true
+    }
+  },
+  {
+    timestamps: false
+  })
+}
