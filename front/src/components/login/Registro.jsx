30a045990a78c634d18dcbf0444e02703d3211a2
Noelia <noelia.martino@live.com.ar>
2023-05-25 14:21:43 -0300

    agrego componente FAQs, login funcional sin conexion con back, estructura formulario de registro

--- front/src/components/login/Registro.jsx
+++ front/src/components/login/Registro.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export const Registro = () => {
     return (
         <div>
             <h2 className='text-center fs-1'>Registro</h2>
             <div className="d-flex justify-content-center m-2">
-                <form className="row g-3 w-75 ">
-                    <div className="col-md-4">
+                <form className="row g-2 md-2 w-75 ">
+                    <div className="col-md-6">
                         <label htmlFor="" className="form-label">Nombre</label>
                         <input type="text" className="form-control" required />
                     </div>
-                    
-                    <div className="col-md-4">
+
+                    <div className="col-md-6">
                         <label htmlFor="" className="form-label">Apellido</label>
                         <input type="text" className="form-control" required />
                     </div>
 
-                    <div className="col-md-4">
+                    <div className="col-md-6">
                         <label htmlFor="" className="form-label">Correo electrónico</label>
                         <div className="input-group">
                             <span className="input-group-text">@</span>
@@ -25,17 +26,36 @@ export const Registro = () => {
                     </div>
 
                     <div className="col-md-6">
-                        <label htmlFor="" className="form-label">Ciudad</label>
-                        <input type="text" className="form-control" id="validationDefault03" required />
+                        <label htmlFor="" className="form-label">Confirmar correo electrónico</label>
+                        <div className="input-group">
+                            <span className="input-group-text">@</span>
+                            <input type="text" className="form-control" required />
+                        </div>
                     </div>
-                    
-                    <div className="col-md-3">
-                        <label htmlFor="" className="form-label">Código postal</label>
-                        <input type="text" className="form-control" id="validationDefault05" required />
+
+                    <div className="col-md-4">
+                        <label htmlFor="" className="form-label">Teléfono (opcional)</label>
+                        <input type="tel" className="form-control" />
                     </div>
-                    
-                    <div className="col-12 d-flex justify-content-center">
+
+                    <div className="col-md-4">
+                        <label htmlFor="" className="form-label">Contraseña</label>
+                        <input type="password" className="form-control" required />
+                    </div>
+
+                    <div className="col-md-4">
+                        <label htmlFor="" className="form-label">Confirmar contraseña</label>
+                        <input type="password" className="form-control" required />
+                    </div>
+
+                    <div className="col-12 d-flex justify-content-center m-4">
                         <button className="btn btn-dark" type="submit">Registrarme</button>
+                    </div>
+
+                    <div className="row text-center m-3 mb-5">
+                        <p className="card-text">
+                            ¿Ya tenés cuenta? <Link to="/ingresar">Ingresá</Link>
+                        </p>
                     </div>
                 </form>
             </div>
