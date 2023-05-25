6bfe2496069c43c385180cdf8f3f67efc2f06127
Noelia <noelia.martino@live.com.ar>
2023-05-25 17:29:26 -0300

    agrego validaciones al registro

---
+++ front/src/components/login/validationRegistro.js
@@ -0,0 +1,40 @@
+const validation = (userInputs, inputsConfirm) => {
+    const errors = {}
+
+    if (userInputs.name === "") {
+        errors.name = "Escribí tu nombre"
+    }
+    if (!/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]*$/.test(userInputs.name)) {
+        errors.name = "Tu nombre no puede contener números ni símbolos"
+    }
+    if (userInputs.lastName === "") {
+        errors.lastName = "Escribí tu apellido"
+    }
+    if (!/^[A-Za-zñÑáéíóúÁÉÍÓÚ\s]*$/.test(userInputs.lastName)) {
+        errors.lastName = "Tu apellido no puede contener números ni símbolos"
+    }
+    if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(userInputs.email)) {
+        errors.email = "Escribí un email válido"
+    }
+    if (userInputs.email !== inputsConfirm.emailC) {
+        errors.emailConfirm = "Los correos no coinciden"
+    }
+    if (userInputs.phone.length > 15 || !/^[0-9]*$/.test(userInputs.phone)) {
+        errors.phone = "Teléfono no válido"
+    }
+    if (userInputs.password === "") {
+        errors.password = "Escribí una contraseña"
+    }
+    if (!/\d/.test(userInputs.password)) {
+        errors.password = "Tu contraseña debe contener al menos un número"
+    }
+    if (userInputs.password.length < 8 || userInputs.password.length > 10) {
+        errors.password = "Tu contraseña debe tener entre 8 y 10 caracteres"
+    }
+    if (userInputs.password !== inputsConfirm.passwordC) {
+        errors.passwordConfirm = "Las contraseñas no coinciden"
+    }
+    return errors
+}
+
+export default validation;
