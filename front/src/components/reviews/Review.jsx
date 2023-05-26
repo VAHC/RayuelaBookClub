024c50ba3b4fb5210aa7ecac70ce10f7a807fc04
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-05-26 18:04:20 -0300

    cambio propiedades egun llegan del back

--- front/src/components/reviews/Review.jsx
+++ front/src/components/reviews/Review.jsx
@@ -4,11 +4,10 @@ import React from 'react';
 const Review = (props) => {
     return (
         <div>
-            <h3>{props.book.title}</h3>
             <h3>{props.title}</h3>
             <h3>{props.qualification}</h3>
             <p>{props.comment}</p>
-            <h3>{props.user.name}</h3>
+            <h3>{props.user}</h3>
         </div>
     )
 };
