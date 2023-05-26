98733a9811015897dacdaf9f96ff615e30794d4a
Andrés Carola <100820494+andrescarola@users.noreply.github.com>
2023-05-26 06:00:29 -0300

    se crea la ruta put y se corrige la ruta create

--- api/src/routes/reviewsRouter.js
+++ api/src/routes/reviewsRouter.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
 const getAllReviewsHandler = require('../handlers/reviews/getAllReviewsHandler');
 const createReviewHandler = require('../handlers/reviews/createReviewHandler');
+const editReviewHandler = require('../handlers/reviews/editReviewHandler');
 
 const reviewsRouter = Router();
 
 reviewsRouter.get('/', getAllReviewsHandler)
 reviewsRouter.post('/', createReviewHandler)
+reviewsRouter.put('/', editReviewHandler)
 
 module.exports = reviewsRouter
