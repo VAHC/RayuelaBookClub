955bb8a9d372b7f712dec574cc57b510b25dd922
Andrés Carola <100820494+andrescarola@users.noreply.github.com>
2023-05-26 06:12:14 -0300

    borrado logico de reviews

--- api/src/routes/reviewsRouter.js
+++ api/src/routes/reviewsRouter.js
@@ -2,11 +2,14 @@ const { Router } = require('express');
 const getAllReviewsHandler = require('../handlers/reviews/getAllReviewsHandler');
 const createReviewHandler = require('../handlers/reviews/createReviewHandler');
 const editReviewHandler = require('../handlers/reviews/editReviewHandler');
+const deleteReviewHandler = require('../handlers/reviews/deleteReviewHandler');
 
 const reviewsRouter = Router();
 
 reviewsRouter.get('/', getAllReviewsHandler)
 reviewsRouter.post('/', createReviewHandler)
 reviewsRouter.put('/', editReviewHandler)
+reviewsRouter.put('/delete/', deleteReviewHandler)
+
 
 module.exports = reviewsRouter
