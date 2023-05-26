e1bc4ffff4c43d9a7d2dc15d93da798c3eaac736
Andrés Carola <100820494+andrescarola@users.noreply.github.com>
2023-05-26 06:00:29 -0300

    se crea la ruta put y se corrige la ruta create

--- api/src/controllers/reviews/createReview.js
+++ api/src/controllers/reviews/createReview.js
@@ -2,22 +2,23 @@ const { Book, Review, User } = require('../../db');
 
 const createReview = async (title, qualification, comment, id_user, id_book) => {
 
+    
+    const user = await User.findByPk(id_user);
+    if (!user) {
+        throw Error(`No user has been found matching the id: ${id_user}`);
+    }
+    
+    const book = await Book.findByPk(id_book);
+    if (!book) {
+        throw Error(`No book has been found matching the id: ${id_book}`);
+    }
+    
     const newReview = await Review.create({
         title,
         qualification,
         comment
     });
-
-    const user = await User.findByPk(id_user);
-    if (!user) {
-        throw Error(`No user has been found matching the id: ${id_user}`);
-    }
-
-    const book = await Book.findByPk(id_book);
-    if (!book) {
-        throw Error(`No book has been found matching the id: ${id_book}`);
-    }
-
+    
     await newReview.setUser(user);
     await newReview.setBook(book);
 
