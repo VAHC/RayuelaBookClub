0a6c5057ebbfef7d8cf2c28093a322b0d65531f5
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-05-25 23:10:14 -0300

    container reviews estilo modal

--- front/src/components/reviews/ContainerReviews.jsx
+++ front/src/components/reviews/ContainerReviews.jsx
@@ -6,11 +6,19 @@ const ContainerReviews = () => {
     const reviewsBook = useSelector(state => state.reviewsBook)
 
     return (
-        <div>
+<div className="modal" tabindex="-1">
+  <div className="modal-dialog modal-dialog-scrollable">
+    <div className="modal-content">
+      <div className="modal-header">
+        <h5 className="modal-title">Reseñas</h5>
+        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body">
+      <div>
             {!reviewsBook.length ? 
             <div>
-            <h3>Aún no hay reseñas...</h3>
-            <h2>Sé el primero en dejar una!</h2>
+            <h6>Aún no hay reseñas...</h6>
+            <h5>Sé el primero en dejar una!</h5>
             </div> 
             : reviewsBook.map(review => {
                 <Review 
@@ -25,6 +33,15 @@ const ContainerReviews = () => {
             })
             }
         </div>
+            
+      </div>
+      <div className="modal-footer">
+        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Cerrar</button>
+        <button type="button" className="btn btn-primary">Deja tu Comentario</button>
+      </div>
+    </div>
+  </div>
+</div>
     )
 
 };
