60c6e37b983cde3a49bbf3b1e7f212cbac25e41c
Noelia <noelia.martino@live.com.ar>
2023-05-28 23:05:22 -0300

    detalles que hablamos en meet y empiezo perfil de user

--- front/src/components/UserDashboard/UserDashboard.jsx
+++ front/src/components/UserDashboard/UserDashboard.jsx
@@ -1,19 +1,19 @@
 import React from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import Menu from "./Menu";
-import MyReviewsContainer from "./myReviews/MyReviewsContainer";
-// import PanelControl from "./PanelControl";
+import { Profile } from "./Profile";
 
 const UserDashboard = () => {
+
     return (
         <Container fluid>
             <Row>
                 <Col xs={2} className="sidebar mb-4">
-                    {/* <PanelControl /> */}
                     <Menu />
                 </Col>
                 <Col xs={10} className="dashboard-content">
-                    <MyReviewsContainer />
+                    {/* <MyReviewsContainer /> */}
+                    <Profile/>
                 </Col>
             </Row>
         </Container>
