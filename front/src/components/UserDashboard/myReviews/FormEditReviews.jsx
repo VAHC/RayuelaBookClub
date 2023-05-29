6af9d889dda07f01d35937f04317223dd95fda84
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-05-29 20:00:04 -0300

    pruebas cierre modal

--- front/src/components/UserDashboard/myReviews/FormEditReviews.jsx
+++ front/src/components/UserDashboard/myReviews/FormEditReviews.jsx
@@ -54,16 +54,30 @@ const FormEditReviews = ({review, toggleModal, handleEditReview}) => {
         });
     };
 
-    const submitHandler = (e) => {
+    // useEffect(() => {
+    //   // Lógica que se ejecutará después de que el estado `success` se haya actualizado
+    //   if (success) {
+    //     console.log(success);
+    //     setTimeout(() => {
+    //       toggleModal();
+    //       setSuccess(false);
+    //     }, 4000);
+    //   }
+    // }, [success]);
+
+    console.log(success)
+    const submitHandler =  (e) => {
         e.preventDefault();
         handleEditReview(review)
         dispatch(putReview(reviewId, input));
         //console.log('se despacha la action');
         //console.log('id del dispatch ' + reviewId);
-        setSuccess(true); // al setearse en true cambia el rederizado          
+        setSuccess(true); // al setearse en true cambia el rederizado 
+        console.log(success)         
         setTimeout(function(){
-            toggleModal()//una vez enviado el form se cierra modal
-            setSuccess(false)
+          toggleModal()//una vez enviado el form se cierra modal
+          setSuccess(false)
+          console.log(success);
         }, 2000)    
     }
 
@@ -111,7 +125,7 @@ const FormEditReviews = ({review, toggleModal, handleEditReview}) => {
                   </div>
                 )}
                 <div className="d-flex flex-row justify-content-evenly">
-                <button className="btn btn-dark m-3"  type="submit">Enviar</button>
+                <button className="btn btn-dark m-3"  type="submit" onClick={submitHandler}>Enviar</button>
                   <button className="btn btn-dark m-3" onClick={() => toggleModal()}>Cerrar</button>
                 </div>
               </form>
