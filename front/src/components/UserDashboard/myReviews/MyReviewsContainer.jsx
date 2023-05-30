05c73d620c7348178326eea69ddd28a9da35274a
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-05-30 00:56:21 -0300

    limpieza de codigo

--- front/src/components/UserDashboard/myReviews/MyReviewsContainer.jsx
+++ front/src/components/UserDashboard/myReviews/MyReviewsContainer.jsx
@@ -7,10 +7,8 @@ import {
     Container,
     Row,
     Col,
-    Form,
     Button,
     Table,
-    Accordion,
 } from "react-bootstrap";
 
 const MyReviewsContainer = () => {
@@ -26,7 +24,7 @@ const MyReviewsContainer = () => {
     const renderStars = (rating) => {
         const stars = [];
         for (let i = 1; i <= 5; i++) {
-          const starIcon = i <= rating ? <i className="bi bi-star-fill" /> : <i className="bi bi-star"/>;
+          const starIcon = i <= rating ? <i key={i} className="bi bi-star-fill" /> : <i key={i} className="bi bi-star"/>;
           stars.push(starIcon);
         }
         return stars;
@@ -122,47 +120,4 @@ const MyReviewsContainer = () => {
     );
 };
 
-export default MyReviewsContainer;
-
-
-// import MyReview from './MyReview';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useEffect } from 'react';
-// import { getReviewsByUser } from '../../../redux/action';
-
-// const MyReviewsContainer = () => {
-//     const userReviews = useSelector((state) => state.userReviews);
-//     //const userId = useSelector((state) => state.user.id);
-//     //user hardcodeado para trabajar
-//     const userId = 1;
-//     const dispatch = useDispatch();
-
-//     useEffect(() => {
-//         dispatch(getReviewsByUser(userId))
-//     }, [userId]);
-
-//     return (
-//         <div>
-//             {!userReviews.length  ? (
-//                 <div>
-//                 <h6>Aún no dejaste una reseña...</h6>
-//                 <h5>Elegi un libro y deja una!</h5>
-//                 </div>
-//             ) : (
-//             userReviews.map((r) => {
-//                 <MyReview
-//                 id={r.id}
-//                 book={r.book}
-//                 title={r.title}
-//                 qualification={r.qualification}
-//                 comment={r.comment}
-//                 key={r.id}
-//                 />
-//             })
-//             )
-//         }
-//         </div>
-//     )
-// };
-
-// export default MyReviewsContainer
+export default MyReviewsContainer
