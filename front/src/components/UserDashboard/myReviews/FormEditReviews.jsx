1610c9233727c2997eddd167fdff55c8e82c3eb2
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-05-30 00:24:53 -0300

    cambios para renderizar imagen en editar reseñas

--- front/src/components/UserDashboard/myReviews/FormEditReviews.jsx
+++ front/src/components/UserDashboard/myReviews/FormEditReviews.jsx
@@ -54,32 +54,42 @@ const FormEditReviews = ({review, toggleModal, handleEditReview}) => {
         });
     };
 
-    useEffect(() => {
-      // Lógica que se ejecutará después de que el estado `success` se haya actualizado
-      if (success) {
-        console.log(success);
-        setTimeout(() => {
-          toggleModal();
-          setSuccess(false);
-        }, 2000);
-      }
-    }, [success]);
+    // useEffect(() => {
+    //   // Lógica que se ejecutará después de que el estado `success` se haya actualizado
+    //   if (success) {
+    //     console.log(success);
+    //     setTimeout(() => {
+    //       toggleModal();
+    //       setSuccess(false);
+    //     }, 2000);
+    //   }
+    // }, [success]);
 
     console.log(success)
-    const submitHandler =  (e) => {
-        e.preventDefault();
-        handleEditReview(review)
-        dispatch(putReview(reviewId, input));
-        //console.log('se despacha la action');
-        //console.log('id del dispatch ' + reviewId);
-        setSuccess(true); // al setearse en true cambia el rederizado 
-        // console.log(success)         
-        // setTimeout(function(){
-        //   toggleModal()//una vez enviado el form se cierra modal
-        //   setSuccess(false)
-        //   console.log(success);
-        // }, 2000)    
-    }
+    // const submitHandler =  (e) => {
+    //     e.preventDefault();
+    //     handleEditReview(review)
+    //     dispatch(putReview(reviewId, input));
+    //     //console.log('se despacha la action');
+    //     //console.log('id del dispatch ' + reviewId);
+    //     setSuccess(true); // al setearse en true cambia el rederizado 
+    //     // console.log(success)         
+    //     // setTimeout(function(){
+    //     //   toggleModal()//una vez enviado el form se cierra modal
+    //     //   setSuccess(false)
+    //     //   console.log(success);
+    //     // }, 2000)    
+    // }
+    const submitHandler = (e) => {
+      e.preventDefault();
+      //  if(formComplete) {
+          dispatch(putReview(reviewId, input));
+          setSuccess(true); // al setearse en true cambia el rederizado  
+          setTimeout(function(){
+            toggleModal()//una vez enviado el form me redirige a reseñas
+              setSuccess(false)
+          }, 2000)    
+      }
 
 
     return (
