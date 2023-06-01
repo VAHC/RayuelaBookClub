197235a7e69243b5acd06846ac4ddb4731c63c5c
VAHC <victor10alha@gmail.com>
2023-06-01 20:21:46 -0300

    second commit

--- api/index.js
+++ api/index.js
@@ -24,10 +24,10 @@ require('dotenv').config()
 const {PORT} = process.env
 
 // Syncing all the models at once.
-conn.sync({ force: true}).then(() => {
+conn.sync({ force: false}).then(() => {
   server.listen(PORT, () => {
     console.log('%s listening at', PORT) // eslint-disable-line no-console
   })
 }).then(async () => {
-  LoadAllBooks() // cargo los generos cuando levanto el server
+  // LoadAllBooks() // cargo los generos cuando levanto el server
 })
