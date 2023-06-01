85a563e259859e98bf10d77ea66a201e881c6b5d
VAHC <victor10alha@gmail.com>
2023-06-01 18:12:59 -0300

    first commit

---
+++ front/ruta.js
@@ -0,0 +1,6 @@
+//export const URL_Railway = 'https://deploy-production-dccf.up.railway.app'
+
+ export const URL_Railway = 'http://localhost:3001'
+
+// Dependiendo del entorno en donde se este trabajando, se descomenta
+// una o la otra.
