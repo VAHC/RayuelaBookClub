cca810ded1617a13f13b6329d2d1f601df3b426d
VAHC <victor10alha@gmail.com>
2023-06-01 20:40:44 -0300

    second commit

--- front/ruta.js
+++ front/ruta.js
@@ -1,6 +1,6 @@
-//export const URL_Railway = 'https://deploy-production-dccf.up.railway.app'
+export const URL_Railway = 'rayuelabookclub-production.up.railway.app'
 
- export const URL_Railway = 'http://localhost:3001'
+// export const URL_Railway = 'http://localhost:3001'
 
 // IMPORTANTE
 // Dependiendo del entorno en donde se este trabajando, se descomenta
