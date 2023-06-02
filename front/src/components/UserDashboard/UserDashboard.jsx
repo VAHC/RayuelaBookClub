eab5a99fe5a41b84f59e8d4c6c2ec6ff85ee161d
LucasFerraro <ferrarolucasmartin@gmail.com>
2023-06-02 17:54:03 -0300

    Ruteo userdashboard funcional

--- front/src/components/UserDashboard/UserDashboard.jsx
+++ front/src/components/UserDashboard/UserDashboard.jsx
@@ -1,20 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 import Menu from "./Menu";
 import { Profile } from "./Profile";
+import { Suscripcion } from "./suscripcion";
 import MyReviewsContainer from "./myReviews/MyReviewsContainer";
 
 const UserDashboard = () => {
+    const [compoActivo, setCompoActivo] = useState("")
+
+    const compoRender= ()=>{
+        if (compoActivo === "profile") return <Profile/>
+        if (compoActivo=== "reviews") return <MyReviewsContainer/>
+        if (compoActivo=== "suscripcion") return <Suscripcion/> 
+    }
 
     return (
         <Container fluid>
             <Row>
                 <Col xs={2} className="sidebar mb-4">
-                    <Menu />
+                    <Menu setCompoActivo={setCompoActivo} />
                 </Col>
                 <Col xs={10} className="dashboard-content">
-                    <Profile/>
-                    <MyReviewsContainer />
+                {compoRender()}
                 </Col>
             </Row>
         </Container>
