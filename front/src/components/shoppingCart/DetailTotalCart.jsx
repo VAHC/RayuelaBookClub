04d1eafd8797f68972af4409eb3938acd0f9018e
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-06-03 02:54:59 -0300

    componentes base del carrito

--- front/src/components/shoppingCart/DetailTotalCart.jsx
+++ front/src/components/shoppingCart/DetailTotalCart.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 
-const DetailTotalCart = () => {
+const DetailTotalCart = (props) => {
+    const totalByitem = (quantity, price) => {
+       return quantity * price
+    } 
+    return (
+        <>
+            <h6>{props.title}</h6>
+            <span>{props.quantity} und</span>
+            <h5>${totalByitem(props.quantity, props.price)}</h5>
+        </>
+    )
 
 }
 
--- front/src/components/shoppingCart/ItemCart.jsx
+++ front/src/components/shoppingCart/ItemCart.jsx
@@ -1,6 +1,24 @@
 import React from "react";
 
-const ItemCart = () => {
+const ItemCart = (props) => {
+    return (
+        <>
+            <img>{props.image}</img>
+            <div>
+                <h5>{props.title}</h5>
+                {props.authors.map(author => {
+                    <h6>{author}</h6>
+                })}
+            </div>
+            <h4>${props.price}</h4>
+            <div>
+                <button><i class="bi bi-arrow-down-square"/></button>
+                <span>{props.quantity}</span>
+                <button><i class="bi bi-arrow-up-square"/></button>
+            </div>
+            <button><i className="bi bi-trash3"/></button>
+        </>
+    )
 
 }
 
