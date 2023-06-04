6c761c2b4a1bbe4b4a3375a02da5175af5e9b117
Noelia <noelia.martino@live.com.ar>
2023-06-04 02:32:21 -0300

    queda listo reset password

---
+++ front/src/components/login/Ingreso.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import { Login } from "./Login";
+import { FormResetPass } from "./FormResetPass";
+
+export const Ingreso = () => {
+
+    const [compoActivo, setCompoActivo] = useState("login")
+
+    const compoRender= ()=>{
+        if (compoActivo === "login") return <Login setCompoActivo={setCompoActivo}/>
+        if (compoActivo=== "formPass") return <FormResetPass setCompoActivo={setCompoActivo}/>
+    }
+
+    return (
+        <div className="d-flex justify-content-center my-3">
+                    <div className="card mb-3 w-75 d-flex justify-content-center">
+                        <div className="row g-0 ">
+                            <div className="col-md-6">
+                                <img src="./images/loginImg.jpg" className="img-fluid rounded-start max-heigth mw-50" alt="Imagen de un libro y un té" />
+                            </div>
+                            <div className="col-md-6">
+                                {compoRender()}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+    )
+}
