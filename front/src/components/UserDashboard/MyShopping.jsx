0f7d65d069c18ad3d27e101e478c4c449740e0df
Virginia Montoya <mvirginiamontoya@yahoo.com.ar>
2023-06-07 03:28:11 -0300

    agrego detalles en propover e iconos por estado

--- front/src/components/UserDashboard/MyShopping.jsx
+++ front/src/components/UserDashboard/MyShopping.jsx
@@ -1,18 +1,22 @@
-import React from "react";
+import React, {useState} from "react";
 import {Link} from 'react-router-dom';
+import ShoppingDetail from "./ShoppingDetail";
 import {
     Container,
     Row,
     Col,
     Table,
+    Popover, 
+    OverlayTrigger
 } from "react-bootstrap";
 
 
 const MyShopping = () => {
+
 const orders = [
     { 
         id : 1,
-        date : '05/05/23, 14:30',
+        date : '05/05/23 - 14:30',
         orderDetail: [{title:'Rayuela', quantity: 1, price: 100}, {title: 'El principito', quantity: 2, price: 50}],
         quantity: 3,
         price_total: '200,00',
@@ -21,7 +25,7 @@ const orders = [
     }, 
     { 
         id : 2,
-        date : '06/06/23, 18:50',
+        date : '06/06/23 - 18:50',
         orderDetail: [{title:'Rayuela', quantity: 2, price: 100}, {title: 'El principito', quantity: 4, price: 50}],
         quantity: 6,
         price_total: '400,00',
@@ -29,6 +33,14 @@ const orders = [
         deleted: false
     },
 ]
+
+const icons = (state) => {
+    if(state === "Created") return <i className="bi bi-pencil-square display-6 text-primary"/>
+    if(state === "Pending") return <i className="bi bi-clock display-6 text-primary"/>
+    if(state === "Cancelled") return <i className="bi bi-x-circle display-6 text-danger"/>
+    if(state === "Completed") return <i className="bi bi-check-circle display-6 text-success"/>
+};
+
 
     return (
         <Container className="min-vh-100">
@@ -60,13 +72,21 @@ const orders = [
                                 {orders.map((order, index) => (
                                     <tr id={order.id} key={index}>
                                         <td>{order.date}</td>
-                                        {/* ver de ponerlo el un btn ver mas o algo similar */}
-                                        {/* sera un map de orderDetail, por ahi directo en esta table, por ahi algo que se habra al hacer hover */}
-                                        <td>Detalle</td>
-                                        <td>{order.quantity} und</td>
+                                        <td>
+                                        <OverlayTrigger trigger="click" placement="right" overlay={
+                                            <Popover>
+                                                <Popover.Body>
+                                                    <ShoppingDetail orderDetail={order.orderDetail} />
+                                                </Popover.Body>
+                                            </Popover>
+                                        }>
+                                            <a  className="text-decoration-none" tabindex="0" href="#">Tu compra...<i className="bi bi-search"/></a>
+                                        </OverlayTrigger>
+                                        </td>
+                                        <td>{order.quantity} und{order.quantity > 1 ? 's' : ''}</td>
                                         <td>${order.price_total}</td>
                                         <td>{order.state}</td>
-
+                                        <td>{icons(order.state)}</td>
                                     </tr>
                                 ))}
                             </tbody>
