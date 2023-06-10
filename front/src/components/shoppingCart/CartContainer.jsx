bf28c26f7e104d6e581567afcc2d8c217e56039f
VAHC <victor10alha@gmail.com>
2023-06-10 00:19:05 -0300

    update

--- front/src/components/shoppingCart/CartContainer.jsx
+++ front/src/components/shoppingCart/CartContainer.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import DetailTotalCart from "./DetailTotalCart";
 import { FormAddress } from "./FormAddress";
