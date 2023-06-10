159ddaa5253dadc345a089714b81ce4481b78ee2
VAHC <victor10alha@gmail.com>
2023-06-09 23:33:41 -0300

    update

--- front/ruta.js
+++ front/ruta.js
@@ -1,7 +1,8 @@
-// export const URL_Railway = 'https://rayuelabookclub-production.up.railway.app'
+export const URL_Railway_front = 'https://rayuelabookclub-production.up.railway.app'
+export const URL_Vercel_front = 'https://rayuela-book-club.vercel.app'
 
-export const URL_Railway = 'http://localhost:3001'
-export const URL_Vercel = 'http://127.0.0.1:5173'
+// export const URL_Railway = 'http://localhost:3001'
+// export const URL_Vercel = 'http://127.0.0.1:5173'
 
 // export const URL_Railway = 'http://localhost:3001'
 // export const URL_Vercel = 'http://127.0.0.1:5173'
