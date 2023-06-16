9e6b13eaa3dff39053e253f9ca04f36523c15a12
Andrés Carola <100820494+andrescarola@users.noreply.github.com>
2023-06-16 00:16:33 -0300

    para deploy

--- api/src/routes/mercadoPagoRouter.js
+++ api/src/routes/mercadoPagoRouter.js
@@ -22,7 +22,7 @@ mpRouter.post('/payment', async (req, res) => {
             unit_price: prod.price
 
         }],
-        notification_url: `https://a51b-2803-9800-b886-82b2-4cbb-fdde-f064-7be.ngrok-free.app/mercadopago/notificar`,
+        notification_url: `https://rayuelabookclub-production.up.railway.app/mercadopago/notificar`,
         back_urls: {
             success: URL_Vercel_back + '/catalogo',
             failure: '',
--- front/ruta.js
+++ front/ruta.js
@@ -1,11 +1,11 @@
 // export const URL_Railway = 'https://rayuelabookclub-production.up.railway.app'
 
-export const URL_Railway = 'http://localhost:3001'
-export const URL_Vercel = 'http://127.0.0.1:5173'
+// export const URL_Railway = 'http://localhost:3001'
+// export const URL_Vercel = 'http://127.0.0.1:5173'
 
 // Para deploy
-// export const URL_Railway = 'https://rayuelabookclub-production.up.railway.app'
-// export const URL_Vercel = 'https://rayuela-book-club.vercel.app'
+export const URL_Railway = 'https://rayuelabookclub-production.up.railway.app'
+export const URL_Vercel = 'https://rayuela-book-club.vercel.app'
 
 
 
